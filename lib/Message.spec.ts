906e159a447501f3980b9be6fb1a9479e6341cff
chemicstry <chemicstry@gmail.com>
2018-03-30 01:54:12 +0300

    Add additional tests

--- lib/Message.spec.ts
+++ lib/Message.spec.ts
@@ -192,6 +192,16 @@ describe('ParseRPCMessage', () => {
         });
     });
 
+    it('Should return a RPCResponseResult when result is empty string', () => {
+        let request = '{"jsonrpc": "2.0", "result": "", "id": 69}';
+
+        expect(ParseRPCMessage(request)).to.deep.equal({
+            jsonrpc: "2.0",
+            result: "",
+            id: 69
+        });
+    });
+
     it('Should return a RPCResponseError when provided with a valid JSON RPC response error object', () => {
         let request = '{"jsonrpc": "2.0", "error": {"code": -32700, "message": "Parse error"}, "id": 69}';
 
